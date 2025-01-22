3929ff834d07e1ed09c961bb1bc3a401cef16a5b
evicsss <evilavictoria@hotmail.com>
2025-01-21 22:56:40 -0300

    corrigindo import

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import Card from "./components/card/Card";
-import Header from './components/Header/Header';
+import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import "./App.scss";
 
